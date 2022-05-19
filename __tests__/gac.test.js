0f171b6e1996e317dae7297909ab3a69be3f9ea0
Timothy Gibson <ryaninlux@gmail.com>
2022-05-19 16:41:07 -0700

    Refactor test for PlanetaryAge

--- __tests__/gac.test.js
+++ __tests__/gac.test.js
@@ -1,15 +1,27 @@
 import PlanetaryAge from '../src/js/planetary-age.js';
 
 describe('PlanetaryAge', () => {
-  let earthAge;
+  let age;
 
-  // beforeEach(() => {
-  //   age = new PlanetaryAge(50, 77)
-  // });
-  test('should receive age and expectancy and add value to propeties', () => {
-    earthAge = new PlanetaryAge(50, 77)
-    expect(earthAge.age).toEqual(50, 77);
-
+  beforeEach(() => {
+    age = new PlanetaryAge(50, 77);
   });
+
+  test('should receive age and add value to age property', () => {
+    expect(age.age).toEqual(50, 77);
+  });
+
+    test('should receive expectancy and add value to the expectancy property', () => {
+      expect(age.expectancy).toEqual(77);
+    });
+  });
+
+
+describe('ageToEarthDays', () => {
+  // let age;
+
+  // test('should return earth age in years into the amount of days')
+  //   age = new PlanetaryAge(50, 80);
+  //   expect()
 });
 
